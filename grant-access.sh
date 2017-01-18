c8a83bc5b0445e0c86562f5f894e7dca63d0d1cd
zevwl <zevwl@bhphoto.com>
2017-01-18 13:47:51 -0500

    Added file grant-access.sh

---
+++ grant-access.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+chmod u+x $1
+echo Granted access to execute file: $1
+
+# Source: http://apple.stackexchange.com/questions/113974/full-privileges-on-command-file-but-it-says-i-dont-have-appropriate-privilege
