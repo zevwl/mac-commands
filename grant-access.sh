7516ba001f7df384af3a6769bf10d20033015334
zevwl <zevwl@bhphoto.com>
2017-03-14 19:43:39 -0400

    Output result of granted file only if exit code is equal to 0

--- grant-access.sh
+++ grant-access.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
 chmod u+x $1
-echo Granted access to execute file: $1
+
+# Print result only if exit code is equals to 0
+if test $? -eq 0;
+then
+  echo Granted access to execute file: $1
+fi
 
 # Source: http://apple.stackexchange.com/questions/113974/full-privileges-on-command-file-but-it-says-i-dont-have-appropriate-privilege
+
